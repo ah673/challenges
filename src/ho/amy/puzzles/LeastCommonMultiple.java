2553929d02168c4331ba01a9e2b110242abcc2ea
Amy Ho <amy.ho@clear-serve.com>
2015-10-29 11:22:09 -0400

    implemented method and unit tests to count element appearance in list

--- src/ho/amy/puzzles/LeastCommonMultiple.java
+++ src/ho/amy/puzzles/LeastCommonMultiple.java
@@ -1,5 +1,34 @@
 package ho.amy.puzzles;
 
-public class LeastCommonMultiple {
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class LeastCommonMultiple {
+	
+	public static int leastCommonFrom1toN( int number ){
+		Map<Integer, Integer> primeCounts = new HashMap<Integer, Integer>();
+		if ( number == 1 ){
+			return number;
+		}
+		List<Integer> primeFactors = PrimeFactorGenerator.primeFactorization( number );
+		Map<Integer, Integer> primeFactorCount = provideCounts( primeFactors );
+		
+		
+	}
+
+	public static Map<Integer, Integer> provideCounts( Collection<Integer> values ) {
+		Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+		for ( int value : values ){
+			int newCount = 1;
+			if( counts.containsKey( value )){
+				int currentCountsForValue = counts.get( value );
+				newCount += currentCountsForValue;
+			}
+			counts.put( value, newCount );
+		}
+		return counts;
+		
+	}
 }
---
+++ src/ho/amy/unittest/ListValueCounter.java
@@ -0,0 +1,46 @@
+package ho.amy.unittest;
+
+import static org.junit.Assert.assertEquals;
+import ho.amy.puzzles.LeastCommonMultiple;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class ListValueCounter {
+	@Test
+	public void countEmptyList(){
+		Map<Integer, Integer> count = LeastCommonMultiple.provideCounts( Collections.<Integer>emptyList() );
+		assertEquals( 0, count.size() ); 
+	}
+	
+	@Test
+	public void countSingle(){
+		List<Integer> list = Arrays.asList( 5 );
+		Map<Integer, Integer> numberCount = LeastCommonMultiple.provideCounts( list );
+		assertEquals( 1, numberCount.size() );
+		
+		int count = numberCount.get( 5 );
+		assertEquals( 1, count );
+	}
+	
+	@Test
+	public void countMultiple(){
+		List<Integer> list = Arrays.asList( 5, 1, 3, 5, 1 );
+		Map<Integer, Integer> numberCount = LeastCommonMultiple.provideCounts( list );
+		assertEquals( 3, numberCount.size() );
+		
+		int count_5 = numberCount.get( 5 );
+		assertEquals( 2, count_5 );
+		
+		int count_3 = numberCount.get( 3 );
+		assertEquals( 1, count_3 );
+		
+		int count_1 = numberCount.get( 1 );
+		assertEquals( 2, count_1 );
+	}
+
+}
