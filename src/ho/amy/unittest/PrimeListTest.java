0f7ce96b94e7be5662b9a2836bc0801870d36643
Amy Ho <amy.ho@clear-serve.com>
2015-10-28 22:55:34 -0400

    Test case for testing prime factorization of prime is self

---
+++ src/ho/amy/unittest/PrimeListTest.java
@@ -0,0 +1,18 @@
+package ho.amy.unittest;
+
+import static org.junit.Assert.*;
+import ho.amy.puzzles.PrimeFactorGenerator;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class PrimeListTest {
+	
+	@Test
+	public void primeFactorsOfPrimeIsSelf(){
+		List<Integer> primeFactors = PrimeFactorGenerator.primeFactorization( 2 );
+		assertEquals(primeFactors.size(), 1 );
+	}
+
+}
