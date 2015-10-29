e56e284c84378b0092efcda5809b8d3fc389f11f
Amy Ho <amy.ho@clear-serve.com>
2015-10-29 07:51:30 -0400

    test cases to find prime factors of non primes

--- src/ho/amy/puzzles/PrimeFactorGenerator.java
+++ src/ho/amy/puzzles/PrimeFactorGenerator.java
@@ -10,10 +10,27 @@ public class PrimeFactorGenerator {
 	public static Set<Integer> primeList = new TreeSet<Integer>( Arrays.asList( 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97) ); 
 	
 	public static List<Integer> primeFactorization( int number ){
-		List<Integer> primeFactors = new ArrayList<Integer>();
+		
+		List<Integer> primeFactors = determineFactorsForNonPrimes( number );
+		return primeFactors;
+	}
+
+	private static List<Integer> determineFactorsForNonPrimes( int number ) {
 		if ( primeList.contains( number )){
-			primeFactors.add( number );
+			return Arrays.asList( number );
+		} 
+		List<Integer> primeFactors = new ArrayList<Integer>();
+		for ( int prime : primeList ){
+			if ( prime * prime > number ){
+				break;
+			} 
+			if ( number % prime == 0 ){
+				primeFactors.add( prime );
+				primeFactors.addAll( determineFactorsForNonPrimes( number/prime ));
+				break;
+			}
 		}
+		
 		return primeFactors;
 	}
 
--- src/ho/amy/unittest/PrimeListTest.java
+++ src/ho/amy/unittest/PrimeListTest.java
@@ -3,6 +3,7 @@ package ho.amy.unittest;
 import static org.junit.Assert.*;
 import ho.amy.puzzles.PrimeFactorGenerator;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -15,6 +16,17 @@ public class PrimeListTest {
 		assertEquals(primeFactors.size(), 1 );
 		int factor = primeFactors.iterator().next();
 		assertEquals( factor, 2 );
+	}
+	
+	@Test
+	public void primeFactorsTest(){
+		List<Integer> primeFactors = PrimeFactorGenerator.primeFactorization( 4 );
+		assertEquals(primeFactors.size(), 2 );
+		assertEquals( primeFactors, Arrays.asList( 2, 2 ));
+		
+		List<Integer> primeFactorsOf_12 = PrimeFactorGenerator.primeFactorization( 12 );
+		assertEquals( primeFactorsOf_12.size(), 3 );
+		assertEquals( primeFactorsOf_12, Arrays.asList( 2, 2, 3 ));
 	}
 
 }
