d50d169ad3d10d119d50e66d6f17288afd59baff
Amy Ho <amy.ho@clear-serve.com>
2015-11-23 09:49:31 -0500

    reversing a linked list

---
+++ src/linkedlist/reverse/ReverseLinkedList.java
@@ -0,0 +1,31 @@
+package linkedlist.reverse;
+
+public class ReverseLinkedList {
+	
+	public ListNode reverseList( ListNode head ){
+		if( head == null || head.next == null ){
+			return head;
+		}
+		ListNode newHead = reverseList( head.next );
+		head.next.next = head;
+		
+		return newHead;
+	}
+	
+	public static class ListNode {
+		int val;
+		ListNode next;
+		ListNode( int x ){ val = x; }
+		
+		@Override
+		public String toString(){
+			return String.valueOf(val);
+		}
+	}
+	
+	public ListNode reverseListIteratively( ListNode head ){
+		
+	}
+
+}
+
---
+++ tests/linkedlist/reverse/ReverseLinkedListTest.java
@@ -0,0 +1,58 @@
+package linkedlist.reverse;
+
+import static org.junit.Assert.*;
+import linkedlist.reverse.ReverseLinkedList.ListNode;
+
+import org.junit.Test;
+
+public class ReverseLinkedListTest {
+
+	@Test
+	public void testReverseOfNoElements() {
+		ReverseLinkedList reverser = new ReverseLinkedList();
+		ListNode reversedList = reverser.reverseList( null );
+		
+		assertEquals( null, reversedList );
+	}
+	
+	@Test
+	public void testReverseOfOneElements() {
+		ReverseLinkedList reverser = new ReverseLinkedList();
+		
+		ListNode n = new ListNode( 5 );
+		
+		ListNode reversedList = reverser.reverseList( n );
+		assertEquals( n, reversedList );
+	}
+	
+	@Test
+	public void testReverseOfTwoElements() {
+		ReverseLinkedList reverser = new ReverseLinkedList();
+		
+		ListNode n = new ListNode( 5 );
+		ListNode next = new ListNode( 6 );
+		n.next = next; 
+		
+		ListNode reversedList = reverser.reverseList( n );
+		assertEquals( next, reversedList );
+	}
+	
+	
+	@Test
+	public void testReverseOfThreeElements() {
+		ReverseLinkedList reverser = new ReverseLinkedList();
+		
+		ListNode n = new ListNode( 5 );
+		ListNode second = new ListNode( 6 );
+		ListNode third = new ListNode( 7 );
+		n.next = second;
+		second.next = third;
+		
+		ListNode reversedList = reverser.reverseList( n );
+		assertEquals( third, reversedList );
+		assertEquals( second, reversedList.next );
+		assertEquals( n, reversedList.next.next );
+	}
+	
+
+}
